9844fc771fff0e3b9d8366c0ba5d1d4da35018d1
Camiliño Calle <camicalle04@gmail.com>
2023-11-06 18:54:43 -0500

    Expanding basics Angular - Part 1

---
+++ Projects/02-basics-angular/src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+  public characters: Character[] = [{
+    name: 'Krilin',
+    power: 1000
+  },{
+    name: 'Goku',
+    power: 9500
+  },{
+    name: 'Vegeta',
+    power: 7500
+  }]
+
+  onDeleteCharacter(index: number) {
+    this.characters.splice(index, 1);
+  }
+
+  onNewCharacter(character: Character):void {
+    this.characters.push(character);
+  }
+
+
+}
+
