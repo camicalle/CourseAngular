1decc94ef6ee9f9f117ddb7dccd7874816e3bb7a
Camiliño Calle <camicalle04@gmail.com>
2023-11-06 19:31:06 -0500

    Expanding basics Angular - part 2 - Service and UUID

--- Projects/02-basics-angular/src/app/dbz/components/list/list.component.ts
+++ Projects/02-basics-angular/src/app/dbz/components/list/list.component.ts
@@ -15,9 +15,10 @@ export class ListComponent {
   }]
 
   @Output()
-  public onDelete: EventEmitter<number> = new EventEmitter();
+  public deleteById: EventEmitter<string> = new EventEmitter();
 
-  onDeleteCharacter(index :number):void {
-    this.onDelete.emit(index);
+  deleteCharacterById(id? :string):void {
+    if (!id ) return;
+    this.deleteById.emit(id);
   }
 }
