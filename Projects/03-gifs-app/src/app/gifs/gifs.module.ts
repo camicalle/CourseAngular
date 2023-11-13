a2cdb1c0eca8e3e24a3923763b21dbbd8aa28116
Camiliño Calle <camicalle04@gmail.com>
2023-11-12 19:24:33 -0500

    Image Loader

--- Projects/03-gifs-app/src/app/gifs/gifs.module.ts
+++ Projects/03-gifs-app/src/app/gifs/gifs.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { HomePageComponent } from './pages/home/home-page.component';
-import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { SharedModule } from '../shared/shared.module';
+
 import { CardListComponent } from './components/card-list/card-list.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { GifsCardComponent } from './components/gifs-card/gifs-card.component';
+import { SearchBoxComponent } from './components/search-box/search-box.component';
 
 @NgModule({
   declarations: [
+    CardListComponent,
     HomePageComponent,
-    SearchBoxComponent,
-    CardListComponent
+    GifsCardComponent,
+    SearchBoxComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     HomePageComponent
